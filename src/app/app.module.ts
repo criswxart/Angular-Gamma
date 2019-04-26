b265486362aefa90904718ac61b862d97f55cfee
Cristian Godoy <crigodoy@alumnos.ubiobio.cl>
2019-04-25 20:34:59 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { APP_ROUTES } from './app.routes';
 import { AppComponent } from './app.component';
+import { LoginModule } from './login/login.module';
+import { HttpClientModule } from '@angular/common/http';
+import { PagesModule} from './pages/pages.module';
+//Servicios
+import { ServiceModule } from './services/service.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
     AppComponent
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    LoginModule,
+    HttpClientModule,
+    PagesModule,
+    APP_ROUTES,
+    ServiceModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,42 @@
+import { CommonModule} from '@angular/common';
+import { NgModule } from "@angular/core";
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { UsuarioComponent } from './usuario/usuario.component';
+import { PAGES_ROUTES } from './pages.routes';
+import { LayoutModule } from '../layout/layout.module';
+import { PagesComponent } from './pages.component';
+import { CreateformComponent } from './usuario/createform.component';
+import { FormsModule } from '@angular/forms';
+import { ProfesionalComponent } from './profesional/profesional.component';
+//paginations
+import {NgxPaginationModule} from 'ngx-pagination';
+//Datepicker
+
+
+
+@NgModule({
+    declarations: [
+        DashboardComponent,
+        UsuarioComponent,
+        PagesComponent,
+        CreateformComponent,
+        ProfesionalComponent
+    ],
+    imports: [
+        PAGES_ROUTES,
+        LayoutModule,
+        CommonModule,
+        FormsModule,
+        NgxPaginationModule
+
+    ],
+    exports: [
+        DashboardComponent,
+        UsuarioComponent,
+        PagesComponent,
+        CreateformComponent
+
+    ]
+})
+
+export class PagesModule {}
